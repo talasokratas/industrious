a08291d03a12ee8b8e1200b1e86b0dffa18fcef3
talasokratas <38657395+talasokratas@users.noreply.github.com>
2018-05-17 09:21:35 +0300

    pradzia

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>SUDETINAS</title>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    </head>
+    
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+        
---
+++ index.php
@@ -0,0 +1,108 @@
+<?php include 'header.php';  ?>
+
+<body>
+        <div class="header_helper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="top_menu">
+                        <a href="#main">Industrious</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="banner">
+                            <div class="inner">
+                            <h1>Industrious<h1>
+                            <p>A responsive business oriented template with a video background<br>
+                            designed by TEMPLATED  and released under the Creative Commons License. </p>    
+                            </div>    
+                        </div>
+                    </div>    
+                </div>
+            </div>
+        </div>
+    <div class="content_main">
+        <div class="container_fluid">
+            <div class="row">
+                <div class="content_header">
+                    <h2>Sem turpis amet semper</h2>
+                    <p>In arcu accumsan arcu adipiscing accumsan orci ac. Felis id enim aliquet.
+                     Accumsan ac integer lobortis commodo ornare aliquet accumsan erat tempus amet porttitor.</p>
+                </div> 
+                <div class="col-md-4">
+                    <div class="content">
+                        <a href="#people" class="fa fa-vcard-o"></a>
+                        <h3>Feugiat consequat</h3>
+                        <p>Nunc lacinia ante nunc ac lobortis ipsum. Interdum
+                        adipiscing gravida odio porttitor sem non mi integer non faucibus.</p>
+                    </div>     
+                </div>  
+                <div class="col-md-4">
+                    <div class="content">
+                        <a href="#people" class="fa fa-files-o"></a>
+                        <h3>Ante Sem Integer</h3>
+                        <p>Nunc lacinia ante nunc ac lobortis ipsum. Interdum
+                        adipiscing gravida odio porttitor sem non mi integer non faucibus.</p>
+                    </div>     
+                </div>
+                <div class="col-md-4">
+                    <div class="content">
+                        <a href="#people" class="fa fa-floppy-o"></a>
+                        <h3>Feugiat consequat</h3>
+                        <p>Nunc lacinia ante nunc ac lobortis ipsum. Interdum
+                        adipiscing gravida odio porttitor sem non mi integer non faucibus.</p>
+                    </div>     
+                </div>         
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="content">
+                        <a href="#people" class="fa fa-line-chart"></a>
+                        <h3>Feugiat consequat</h3>
+                        <p>Nunc lacinia ante nunc ac lobortis ipsum. Interdum
+                        adipiscing gravida odio porttitor sem non mi integer non faucibus.</p>
+                    </div>     
+                </div>  
+                <div class="col-md-4">
+                    <div class="content">
+                        <a href="#people" class="fa fa-paper-plane-o"></a>
+                        <h3>Feugiat consequat</h3>
+                        <p>Nunc lacinia ante nunc ac lobortis ipsum. Interdum
+                        adipiscing gravida odio porttitor sem non mi integer non faucibus.</p>
+                    </div>     
+                </div>
+                <div class="col-md-4">
+                    <div class="content">
+                        <a href="#people" class="fa fa-qrcode"></a>
+                        <h3>Feugiat consequat</h3>
+                        <p>Nunc lacinia ante nunc ac lobortis ipsum. Interdum
+                        adipiscing gravida odio porttitor sem non mi integer non faucibus.</p> 
+                    </div> 
+                    <div style="margin-bottom: 120px;"></div>     
+                </div>         
+            </div>  
+              
+        </div>    
+    </div>    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ <?php include 'footer.php';  ?>
